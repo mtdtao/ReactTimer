ed9b00c7bb47f71b0271f5fd82caa30ad28c09d6
mtdtao <mtdtao@gmail.com>
2016-07-06 21:40:57 -0400

    finished countdown

--- app/tests/components/Clock.test.jsx
+++ app/tests/components/Clock.test.jsx
@@ -9,5 +9,35 @@ var Clock = require('Clock');
 describe('Clock', () => {
     it('should exist', () => {
         expect(Clock).toExist();
-    })
+    });
+
+    describe('render', () => {
+        it('should render clock to output', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock totalSeconds={62}/>);
+            var $el = $(ReactDOM.findDOMNode(clock));
+            var actualText = $el.find('.clock-text').text();
+
+            expect(actualText).toBe('01:02');
+        });
+    });
+
+    describe('formatSeconds', () => {
+        it('should format seconds', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 615;
+            var expected = '10:15';
+            var actual = clock.formatSeconds(seconds);
+
+            expect(actual).toBe(expected);
+        });
+
+        it('should format seconds for less then 10 min', () => {
+            var clock = TestUtils.renderIntoDocument(<Clock/>);
+            var seconds = 75;
+            var expected = '01:15';
+            var actual = clock.formatSeconds(seconds);
+
+            expect(actual).toBe(expected);
+        });
+    });
 });
---
+++ app/tests/components/Countdown.test.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var Countdown = require('Countdown');
+
+describe('Countdown', () => {
+    it('should exist', () => {
+        expect(Countdown).toExist();
+    });
+
+    it('handleSetCountdown', (done) => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown.handleSetCountdown(10);
+
+        expect(countdown.state.count).toBe(10);
+        expect(countdown.state.countdownStatus).toBe('started');
+
+        setTimeout(() => {
+            expect(countdown.state.count).toBe(9);
+            done();
+        }, 1001);
+    });
+
+    it('should never set timer under 0', (done) => {
+        var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+        countdown.handleSetCountdown(1);
+
+        setTimeout(() => {
+            expect(countdown.state.count).toBe(0);
+            done();
+        }, 3001);
+    });
+});
